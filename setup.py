113f8a719aef1e52c86fe1cc9f430a98c80f2d1b
Robb Northrup <89609223+NorthrupRobert@users.noreply.github.com>
2022-07-24 21:11:57 -0700

    init

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='StatPy',
+setup(name='Nightingale',
       version='0.1',
       description='A plugin that computes statistical data using basic models.',
-      url='https://github.com/NorthrupRobert/StatPy',
+      url='https://github.com/NorthrupRobert/Nightingale',
       author='Robb Northrup',
       author_email='RobbNorthrup@outlook.com',
       license='MIT',
-      packages=['StatPy'],
+      packages=['NightinGale'],
       zip_safe=False)
