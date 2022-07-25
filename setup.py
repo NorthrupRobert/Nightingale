1aaab6b5490730570d5d524238b1f62c6cecc666
NorthrupRobert <vadusoft@gmail.com>
2022-07-24 20:30:00 -0700

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='StatPy',
+      version='0.1',
+      description='A plugin that computes statistical data using basic models.',
+      url='https://github.com/NorthrupRobert/StatPy',
+      author='Robb Northrup',
+      author_email='RobbNorthrup@outlook.com',
+      license='MIT',
+      packages=['StatPy'],
+      zip_safe=False)
