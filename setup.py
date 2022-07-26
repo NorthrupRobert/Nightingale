390c6a8d8b5aceaee5f685fcc7b9a182f1d52c7e
Robb Northrup <89609223+NorthrupRobert@users.noreply.github.com>
2022-07-26 15:09:45 -0700

    0.1.0
    Code base implemented, needs restructuring and improvements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nightingale',
+      version='0.1',
+      description='A python library for statistical and scientific computations.',
+      url='https://github.com/NorthrupRobert/nightingale',
+      author='Robb Northrup',
+      author_email='RobbNorthrup@outlook.com',
+      license='MIT',
+      packages=['nightingale'],
+      zip_safe=False)
